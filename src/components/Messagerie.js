c76d3e14fdc6eaed49d080b526c7d78cff3c4898
WalterX85 <cedric.guyot2@gmail.com>
2021-08-31 16:28:03 +0200

    darkmode complet

--- src/components/Messagerie.js
+++ src/components/Messagerie.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useContext}from 'react'
+import {ThemeContext} from '../context/ThemeContext'
+import BtnDarkMode from './BtnDarkMode';
+
 
 function Messagerie() {
+
+  const {darkMode} = useContext(ThemeContext)
+
+  console.log(darkMode);
+
     return (
         <div>
             <h1>Welcome to messagerie</h1>
+            <BtnDarkMode />
         </div>
     )
 }
--- src/components/Parametres.js
+++ src/components/Parametres.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import {ThemeContext} from '../context/ThemeContext'
+import BtnDarkMode from './BtnDarkMode';
 
 function Parametres() {
+
+  const {darkMode} = useContext(ThemeContext)
+
+  console.log(darkMode);
+
     return (
         <div>
             <h1>Hello from parametres</h1>
+            <BtnDarkMode />
         </div>
     )
 }
