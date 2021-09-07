cea0f1e4e3364dfb2ef669647be823c68d51a972
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-09-07 09:17:51 +0200

    Fixed navBar

--- src/components/Navigation/NavbarData.js
+++ src/components/Navigation/NavbarData.js
@@ -5,40 +5,40 @@ import * as ImIcons from 'react-icons/im';
 import * as FaIcons from 'react-icons/fa';
 
 export const NavbarData = [
-  {
-    title: 'MonEspace',
-    path: '/',
-    icon: <FaIcons.FaUser />,
-    cName: 'nav-text',
-    func: 1
-  },
+  // {
+  //   title: 'MonEspace',
+  //   path: '/',
+  //   icon: <FaIcons.FaUser />,
+  //   cName: 'nav-text',
+  //   func: 1
+  // },
   {
     title: 'Message',
     path: '/messagerie',
     icon: <MdIcons.MdMessage />,
     cName: 'nav-text',
-    func: 0
+    // func: 0
   },
   {
     title: 'Mon Fil d actualité',
     path: '/monfil',
     icon: <ImIcons.ImNewspaper />,
     cName: 'nav-text',
-    func: 0
+    // func: 0
   },
   {
     title: 'Carte',
     path: '/carte',
     icon: <FaIcons.FaMapMarkerAlt />,
     cName: 'nav-text',
-    func: 0
+    // func: 0
   },
   {
     title: 'Parametres',
     path: '/parametres',
     icon: <FiIcons.FiSettings />,
     cName: 'nav-text',
-    func: 0
+    // func: 0
   },
 
 ];
