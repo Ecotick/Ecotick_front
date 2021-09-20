af1562fca190310a1dbc0b8351b985d37bc650e2
WalterX85 <cedric.guyot2@gmail.com>
2021-09-20 11:33:48 +0200

    html-form

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,39 @@ function Home() {
 
     return (
         <div>
-            <h1>Accueil</h1>
+            <h1 className='titreForm'>Login</h1>
+
+            <section className="formulaire">
+
+              <form id="login" className="visible">
+
+                <div className="form">
+                  <label for="email">eMail</label>
+                  <input type="text" id="email" name="email" placeholder="Votre eMail..."></input> <br/>
+                  <label for="pass">password</label>
+                  <input type="password" id="pass" name="pass" placeholder="Votre password.."></input> <br/>
+                  <button className="submmit">Submit</button>
+                </div>
+
+              </form>
+              
+              <h1 className='titreForm'>Register</h1>
+              
+              <form id="register">
+
+                <div className="form">
+                  <label for="email">eMail</label>
+                  <input type="text" id="email" name="email" placeholder="Votre eMail..."></input> <br/>
+                  <label for="pass">password</label>
+                  <input type="password" id="pass" name="pass" placeholder="Votre password.."></input> <br/>
+                  <label for="pass">confirm password</label>
+                  <input type="password" id="pass" name="pass" placeholder="Votre password.."></input> <br/>
+                  <button className="submmit">Submit</button>
+                </div>
+
+                </form>
+
+            </section>
         </div>
     )
 }
