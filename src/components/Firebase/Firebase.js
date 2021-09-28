2be084016e4f207210655cb935bd6a21dcbd13dc
WalterX85 <cedric.guyot2@gmail.com>
2021-09-28 09:25:33 +0200

    config firebase

---
+++ src/components/Firebase/Firebase.js
@@ -0,0 +1,23 @@
+import app from 'firebase/app';
+
+const config = {
+
+  apiKey: "AIzaSyBW6yP4Q2tiX-6KwuU9qe9u5RCpmdGlBDU",
+  authDomain: "ecotick-29526.firebaseapp.com",
+  projectId: "ecotick-29526",
+  storageBucket: "ecotick-29526.appspot.com",
+  messagingSenderId: "291528898510",
+  appId: "1:291528898510:web:86d094dbbb422e172fdd9f",
+  measurementId: "G-K974E0ZYG4"
+
+};
+
+
+
+class Firebase {
+  constructor(){
+    app.initializeApp(config);
+  }
+}
+
+export default Firebase;
